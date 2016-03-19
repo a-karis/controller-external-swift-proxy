c01b03e9b27e56a461d6bfbdde372e0c688ecc62
akaris <akaris@redhat.com>
2016-03-19 18:00:17 -0400

    workaround for https://bugzilla.redhat.com/show_bug.cgi?id=1312007

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-openstack overcloud deploy --templates -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml -e /home/stack/templates/networks-additional/networks-additional-isolation.yaml -e /home/stack/templates/network-environment.yaml -e /home/stack/templates/external-swift-proxy/controller-swift-proxy-registry.yaml --control-flavor control --compute-flavor compute --control-scale 1 --compute-scale 1 --ceph-storage-scale 0 --ntp-server 10.5.26.10 --neutron-network-type vxlan --neutron-tunnel-types vxlan
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+template_base_dir="$DIR"
+
+random=$(uuidgen)
+# just to avoid that we replace in this script itself ... just in case it resides in template_base_dir ...
+uuid_marker="UUID-"
+find $template_base_dir -type f | xargs -I {} sed -i "s/###${uuid_marker}.*/###${uuid_marker}$random/g" {}
+
+openstack overcloud deploy --templates -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml -e {template_base_dir}/networks-additional/networks-additional-isolation.yaml -e ${template_base_dir}/network-environment.yaml -e ${template_base_dir}/external-swift-proxy/controller-swift-proxy-registry.yaml --control-flavor control --compute-flavor compute --control-scale 1 --compute-scale 1 --ceph-storage-scale 0 --ntp-server 10.5.26.10 --neutron-network-type vxlan --neutron-tunnel-types vxlan
